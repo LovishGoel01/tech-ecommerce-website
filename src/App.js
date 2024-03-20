bd3fb31a1e432bdd32549bccd865f51ac191e5b2
lovish goel <lovish.goel2000@gmail.com>
2024-03-20 22:30:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ import Newsletter from "./components/Footer/Newsletter/Newsletter";
 import AppContext from "./utils/context";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <AppContext>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/category/:id" element={<Category />} />
-                    <Route path="/product/:id" element={<SingleProduct />} />
-                </Routes>
-                <Newsletter />
-                <Footer />
-            </AppContext>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <AppContext>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+        </Routes>
+        <Newsletter />
+        <Footer />
+      </AppContext>
+    </BrowserRouter>
+  );
 }
 
 export default App;
